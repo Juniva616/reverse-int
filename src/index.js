9d2c65780d4c5b5fd034fe48d0ce73088547872f
Juniva616 <juniva616@gmail.com>
2023-02-20 21:46:39 +0000

    feat: reverse numbers including negative

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function reverse (n) {
-  
+function reverse(num) {
+    let number = num * Math.sign(num);
+    let digitsArray = [];  
+    while (number  > 0 ) {
+      digitsArray.push(number % 10);
+      number = Math.floor(number / 10);
+    }
+    const x = Number(digitsArray.join(""));
+    return x;  
 }
+
+ 
+
+module.exports = reverse; 
+  
+ 
+
+
